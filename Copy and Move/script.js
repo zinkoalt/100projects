b49768e74df311bad360bf17d82aab4626c5d24d
azdine ham <azdevkode@gmail.com>
2025-03-24 23:44:50 +0100

    Copy and Move

---
+++ Copy and Move/script.js
@@ -0,0 +1,45 @@
+//copyText
+const copyText = document.querySelector('textarea[name=copyTxt');
+//finalText
+const finalText = document.querySelector('textarea[name=finalTxt');
+//moveBtn
+const moveBtn = document.querySelector('.moverBtn');
+//copyBtn
+const copyBtn = document.querySelector('.copyBtn');
+//output
+const output = document.querySelector('.output');
+
+copyBtn.addEventListener("click", () => {
+    let temp = copyText.value;
+    copyToClipBoard(temp);
+});
+
+copyBtn.addEventListener("click", () => {
+    let temp = copyText.value;
+    copyToClipBoard(temp);
+});
+
+moveBtn.addEventListener("click", () => {
+    let temp = copyText.value;
+    finalText.value = temp; 
+});
+
+copyText.addEventListener("click", () => this.select());
+finalText.addEventListener("click", () => this.select());
+
+
+function copyToClipBoard(str) {
+    const outputContainer = document.querySelector('.output-container');
+    const textarea = document.createElement('textarea');
+    textarea.value = str;
+    outputContainer.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    outputContainer.removeChild(textarea);
+    output.innerHTML = '<h3>Content copied </h3>';
+    output.classList.add('added');
+    setTimeout(() => {
+        output.classList.toggle('added')
+        output.textContent = '';
+    }, 2000);
+}
