516c911e8d29503d89fedf933c44555666435a9d
azdine ham <azdevkode@gmail.com>
2025-03-30 22:17:16 +0200

    Progress Steps

---
+++ Progress Steps/script.js
@@ -0,0 +1,59 @@
+// progress
+const progress = document.getElementById("progress");
+// prevBtn
+const prevBtn = document.getElementById("prev");
+// nextBtn
+const nextBtn = document.getElementById("next");
+// circles
+const circles = document.querySelectorAll(".circle");
+
+//console.log(progress);
+//console.log(prevBtn);
+//console.log(nextBtn);
+//console.log(circles);
+
+let activeIndex = 1;
+
+nextBtn.addEventListener("click", () => {
+    activeIndex++;
+
+    if (activeIndex > circles.length) {
+        activeIndex = circles.length;
+    }
+
+    updateUI();
+});
+prevBtn.addEventListener("click", () => {
+    activeIndex--;
+
+    if (activeIndex < 1) {
+        activeIndex = 1;
+    }
+
+    updateUI();
+});
+
+
+function updateUI() {
+    circles.forEach((circle, index) => {
+        if (index < activeIndex) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    });
+
+    const actives = document.querySelectorAll(".active");
+
+    progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+    if (activeIndex === 1) {
+        prevBtn.disabled = true;
+    }
+    else if (activeIndex === circles.length) {
+        nextBtn.disabled = true;
+    } else {
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
+    }
+}
