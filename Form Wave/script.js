eac9faebe7c1b0e9324d0503f82c5dd55e0c43a0
azdine ham <azdevkode@gmail.com>
2025-08-21 17:16:53 +0200

    Form Wave Animation

---
+++ Form Wave/script.js
@@ -0,0 +1,12 @@
+
+const allLabels = document.querySelectorAll(".form-control label");
+
+allLabels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
