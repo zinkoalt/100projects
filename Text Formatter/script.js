f7a7f891262cad4368551989fffc7472f9fcb6cf
azdine ham <azdevkode@gmail.com>
2026-04-24 23:45:31 +0200

    Text Formatter

---
+++ Text Formatter/script.js
@@ -0,0 +1,25 @@
+const inputField = document.getElementById('input-field');
+const outputField = document.getElementById('output-field');
+const buttons = document.querySelectorAll('button');
+
+
+inputField.addEventListener("keyup" , () => outputField.innerHTML = inputField.value);
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click" , () => {
+        if (btn.classList.contains("uppercase")) {
+            outputField.innerHTML = outputField.innerHTML.toUpperCase();
+        }  else if (btn.classList.contains("lowercase")) {
+            outputField.innerHTML = outputField.innerHTML.toLowerCase();
+        }  else if (btn.classList.contains("capitalize")) {
+            outputField.innerHTML = outputField.innerHTML.charAt(0).toUpperCase() + outputField.innerHTML.slice(1).toLocaleLowerCase();
+        }  else if (btn.classList.contains("bold")) {
+            outputField.style.fontWeight = "900";
+        }  else if (btn.classList.contains("italic")) {
+            outputField.style.fontStyle = "italic";
+        }  else if (btn.classList.contains("underline")) {
+            outputField.style.textDecoration = "underline";
+        }
+        
+    });
+});
